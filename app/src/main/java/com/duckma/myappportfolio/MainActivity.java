9d7579d864cb470f15bce8faee1a6b64baaa4cdd
Matteo Gazzurelli <info@duckma.com>
2015-07-18 15:24:22 +0200

    Updated project after feedback

--- app/src/main/java/com/duckma/myappportfolio/MainActivity.java
+++ app/src/main/java/com/duckma/myappportfolio/MainActivity.java
@@ -2,12 +2,12 @@ package com.duckma.myappportfolio;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Toast mToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,31 +22,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.btn_main_capstone).setOnClickListener(this);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 
     @Override
     public void onClick(View v) {
+        if(mToast != null ) mToast.cancel();
+
         String text = "";
         switch (v.getId()){
             case R.id.btn_main_streamer:
@@ -71,6 +51,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
 
-        Toast.makeText(this,text,Toast.LENGTH_SHORT).show();
+        mToast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+        mToast.show();
     }
 }
